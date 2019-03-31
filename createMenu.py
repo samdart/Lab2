aefa5d1d39eef4c2135f8712f4cd0b958114ad4e
Sam Dart <dartsa@mnstate.edu>
2019-03-31 14:17:05 -0500

    created test for createMenu, added doc

--- createMenu.py
+++ createMenu.py
@@ -1,5 +1,10 @@
 #
 def createMenu(optionList):
+  """
+  Precondition:optionList is a list of strings
+  Postcondition: each item in optionList is printed with formatting
+  Description: Prints a list of stringss in a menu format
+  """
   tmp = ' '; ct = 0
   for opt in optionList:
     tmp += str(ct) + '.' + opt + '\n'
---
+++ testMenu.py
@@ -0,0 +1,8 @@
+from createMenu import *
+
+
+optionList1 = ['Add','Sub','Mult','Div','Quit']
+optionList2 = ['Deposit','Withdraw','Close','Check Balance','Quit']
+
+print(createMenu(optionList1))
+print(createMenu(optionList2))
